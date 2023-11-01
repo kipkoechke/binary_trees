183ae3ac5868003143944800fd1dd60913108b51
kipkoechke <chepkwonyke1@gmail.com>
2023-11-01 08:58:22 -0700

    Fixd - Betty style issues

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,8 +7,8 @@
  * Return: If the node is the root, return 1.
  *         Otherwise, return 0.
  *
- * Description: This function determines if a given node is the root in a binary
- * tree. A root node is one that has no parent.
+ * Description: This function determines if a given node is the root in a
+ * binary tree. A root node is one that has no parent.
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
