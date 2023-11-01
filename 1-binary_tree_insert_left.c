a63ed8b32157f40d3b2b8e6c4fa09a556cd1b29f
kipkoechke <chepkwonyke1@gmail.com>
2023-11-01 08:44:59 -0700

    Fixed - Betty style issues

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Insert a node as the left child of another in a binary tree.
+ * binary_tree_insert_left - Insert a node as the left child of another
+ *                         in a binary tree.
  * @parent: A pointer to the node to insert the left child in.
  * @value: The value to store in the new node.
  *
@@ -14,21 +15,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
-    parent->left = new_node;
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
 
-    return (new_node);
+	return (new_node);
 }
